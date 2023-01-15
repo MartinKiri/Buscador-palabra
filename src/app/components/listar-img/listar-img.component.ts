f5e7924ae56fb6bfc5940d63f12e898471dc6920
Mart0 <martin.kiri98@gmail.com>
2023-01-16 00:14:44 +0100

    implementeción de paginación, falta editar css pero funciona 100%

--- src/app/components/listar-img/listar-img.component.ts
+++ src/app/components/listar-img/listar-img.component.ts
@@ -12,10 +12,14 @@ export class ListarImgComponent implements OnInit {
   suscription: Subscription;
   listImagenes: any[] = [];
   loading= false;
+  imgXPaginas= 25;
+  paginaActu=1;
+  totalPaginas=0;
 
   constructor(private _imagenService: ImagenService) {
     this.suscription = this._imagenService.getBusqueda().subscribe((data) => {
       this.termino = data;
+      this.paginaActu=1
       this.loading=true
       console.log(this.termino);
       this.obtenerImg();
@@ -25,7 +29,7 @@ export class ListarImgComponent implements OnInit {
   ngOnInit(): void {}
 
   obtenerImg() {
-    this._imagenService.getImagenes(this.termino).subscribe(
+    this._imagenService.getImagenes(this.termino, this.imgXPaginas,this.paginaActu).subscribe(
       (data) => {
         console.log(data);
         this.loading=false
@@ -34,6 +38,7 @@ export class ListarImgComponent implements OnInit {
           this._imagenService.setError('No Encontramos un resultado');
           return;
         }
+        this.totalPaginas= Math.ceil(data.totalHits/ this.imgXPaginas)
         this.listImagenes= data.hits
       },
       (error) => {
@@ -41,5 +46,32 @@ export class ListarImgComponent implements OnInit {
         this._imagenService.setError('Ocurrio un error');
       }
     );
+  }
+  anterior(){
+    this.paginaActu--;
+    this.loading=true;
+    this.listImagenes=[]
+    this.obtenerImg()
+  }
+  siguiente(){
+    this.paginaActu++;
+    this.loading=true;
+    this.listImagenes=[]
+    this.obtenerImg()
+  }
+
+  anteriorClass(){
+    if (this.paginaActu=== 1) {
+      return false
+    }else{
+      return true
+    }
+  }
+  siguienteClass(){
+    if (this.paginaActu == this.totalPaginas) {
+      return false
+    }else{
+      return true
+    }
   }
 }
--- src/app/services/imagen.service.ts
+++ src/app/services/imagen.service.ts
@@ -1,34 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable,Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ImagenService {
-  private error$ = new Subject <string>();
-  private terminoBusqueda$ =  new Subject <string>()
+  private error$ = new Subject<string>();
+  private terminoBusqueda$ = new Subject<string>();
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
-  setError(mensaje: string){
+  setError(mensaje: string) {
     this.error$.next(mensaje);
   }
-  getError():Observable<string> {
+  getError(): Observable<string> {
     return this.error$.asObservable();
   }
 
-  enviarBusqueda(termino:string){
+  enviarBusqueda(termino: string) {
     this.terminoBusqueda$.next(termino);
   }
 
-  getBusqueda():Observable<string>{
-    return this.terminoBusqueda$.asObservable()
+  getBusqueda(): Observable<string> {
+    return this.terminoBusqueda$.asObservable();
   }
 
-  getImagenes(termino:string):Observable<any>{
-    const KEY='32876415-5fc00f0ac4ce0465058532327'
-    const URL='https://pixabay.com/api/?key='+ KEY +'&q='+ termino;
-    return this.http.get(URL)
+  getImagenes(
+    termino: string,
+    imgXPaginas: number,
+    paginaActu: number
+  ): Observable<any> {
+    const KEY = '32876415-5fc00f0ac4ce0465058532327';
+    const URL = 'https://pixabay.com/api/?key='+KEY +'&q=' +termino +                '&per_page='+ imgXPaginas + '&page='+paginaActu;
+
+    return this.http.get(URL);
   }
 }
